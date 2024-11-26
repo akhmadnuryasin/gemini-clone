27571094ddc97f2ec40b40b9fce140d0cecdfe26
akhmadnuryasin <akhmadnuryasin18@gmail.com>
2024-11-27 04:26:34 +0700

    finish project and vercel configuration

---
+++ src/config/gemini.js
@@ -0,0 +1,39 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+
+if (!apiKey) {
+  throw new Error("API Key not found. Please make sure it's defined in .env file.");
+}
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  try {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  } catch (error) {
+    console.error("Error in generating content:", error);
+    throw new Error("Failed to fetch from API. Please check the API key and network.");
+  }
+}
+
+export default run;
